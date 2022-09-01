89e02f71df61b643fe7adc1965361398361af532
ozzzzz <you@example.com>
2022-09-02 02:52:57 +0300

    gunicorn requirements add

--- bookshelf/settings.py
+++ bookshelf/settings.py
@@ -141,7 +141,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 
