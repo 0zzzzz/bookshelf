2a024feb4aa504d17773e1d79384c3581da4a9e2
ozzzzz <you@example.com>
2022-09-02 17:22:07 +0300

    phone check add

--- static/js/register.js
+++ static/js/register.js
@@ -10,7 +10,7 @@ console.log(form)
 
 const url = ""
 
-avatar.addEventListener('change', ()=>{
+avatar.addEventListener('change', () => {
     const avatar_data = avatar.files[0]
     const url = URL.createObjectURL(avatar_data)
     console.log(url)
@@ -18,79 +18,88 @@ avatar.addEventListener('change', ()=>{
 })
 
 
-jQuery.validator.addMethod("noSpace", function(value, element) {
+$.validator.addMethod("noSpace", function (value, element) {
     return value == '' || value.trim().length != 0;
 }, "Не используйте пробелы и не оставляйте пустым");
-jQuery.validator.addMethod("customEmail", function(value, element) {
-  return this.optional( element ) || /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test( value );
+$.validator.addMethod("customEmail", function (value, element) {
+    return this.optional(element) || /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(value);
 }, "Введите правильный email");
-$.validator.addMethod( "alphanumeric", function( value, element ) {
-return this.optional( element ) || /^\w+$/i.test( value );
-}, "Используйте только латинские буквы, цифры и нижнее подчеркивания" );
-$.validator.addMethod( "fullName", function( value, element ) {
-return this.optional( element ) || /^[А-Яа-яA-Za-z]+$/i.test( value );
-}, "Используйте только буквы" );
+$.validator.addMethod("alphanumeric", function (value, element) {
+    return this.optional(element) || /^\w+$/i.test(value);
+}, "Используйте только латинские буквы, цифры и нижнее подчеркивания");
+$.validator.addMethod("fullName", function (value, element) {
+    return this.optional(element) || /^[А-Яа-яA-Za-z]+$/i.test(value);
+}, "Используйте только буквы");
+$.validator.addMethod("phoneNumber", function (value, element) {
+    return this.optional(element) || /^[0-9\+]{1,}[0-9\-]{10,11}$/.test(value);
+}, "Только цифры и знак +");
 var $registrationForm = $('#reg-form');
-if($registrationForm.length){
-  $registrationForm.validate({
-      rules:{
-          username: {
-              required: true,
-              alphanumeric: true
-          },
-          email: {
-              required: true,
-              customEmail: true
-          },
-          password1: {
-              // required: true
-              minlength: 8
-          },
-          password2: {
-              // required: true,
-              minlength: 8,
-              equalTo: '[name="password1"]'
-          },
-          first_name: {
-              required: true,
-              fullName: true
-          },
-          last_name: {
-              required: true,
-              fullName: true
-          },
-          patronymic: {
-              required: true,
-              fullName: true
-          }
-      },
-      messages:{
-          username: {
-              required: 'Введите имя пользователя'
-          },
-          email: {
-              required: 'Введите email',
-              email: 'Введите правильный email'
-          },
-          password1: {
-              required: 'Введите пароль',
-              minlength: 'Минимальная длинна пароля 8 символов'
-          },
-          password2: {
-              required: 'Введите пароль повторно',
-              equalTo: 'Пароли не совпадают',
-              minlength: 'Минимальная длинна пароля 8 символов'
-          },
-          first_name: {
-              required: 'Введите имя'
-          },
-          last_name: {
-              required: 'Введите фамилию'
-          },
-          patronymic: {
-              required: 'Введите отчество'
-          }
-      }
-  });
+if ($registrationForm.length) {
+    $registrationForm.validate({
+        rules: {
+            username: {
+                required: true,
+                alphanumeric: true
+            },
+            email: {
+                required: true,
+                customEmail: true
+            },
+            password1: {
+                // required: true
+                minlength: 8
+            },
+            password2: {
+                // required: true,
+                minlength: 8,
+                equalTo: '[name="password1"]'
+            },
+            phone: {
+                phoneNumber: true
+            },
+            first_name: {
+                required: true,
+                fullName: true
+            },
+            last_name: {
+                required: true,
+                fullName: true
+            },
+            patronymic: {
+                required: true,
+                fullName: true
+            }
+        },
+        messages: {
+            username: {
+                required: 'Введите имя пользователя'
+            },
+            email: {
+                required: 'Введите email',
+                email: 'Введите правильный email'
+            },
+            password1: {
+                required: 'Введите пароль',
+                minlength: 'Минимальная длинна пароля 8 символов'
+            },
+            password2: {
+                required: 'Введите пароль повторно',
+                equalTo: 'Пароли не совпадают',
+                minlength: 'Минимальная длинна пароля 8 символов'
+            },
+            phone: {
+                phoneNumber: 'Введите правильный телефон',
+            },
+            first_name: {
+                required: 'Введите имя'
+            },
+            last_name: {
+                required: 'Введите фамилию'
+            },
+            patronymic: {
+                required: 'Введите отчество'
+            }
+        }
+    });
 }
 
