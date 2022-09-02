69a555193ddf7e78becfb9eac6bc2483a939054e
ozzzzz <you@example.com>
2022-09-02 03:01:04 +0300

    urls fix

--- bookshelf/urls.py
+++ bookshelf/urls.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 from mainapp import views as mainapp
 
 urlpatterns = [
@@ -12,4 +10,3 @@ urlpatterns = [
 
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
